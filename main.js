27d566269d4c7ffad72e395843d4d73cc5663262
Florencia Rodriguez <flor2702@live.com.ar>
2023-10-01 21:14:13 -0300

    correccion entrega1

--- main.js
+++ main.js
@@ -1,62 +1,60 @@
-let producto = prompt("De cuantos pisos desea comprar la torta?")
-while(producto) {
-    switch (producto){
-        case "1":
-            alert ("Eligio la torta de un piso")
-        let preguntacuotas = prompt("En cuantas cuotas desea la compra?");
-if (preguntacuotas < 2) {
-    alert ("El costo en una sola cuota es de 10.000 pesos")
-    
-} 
-else if (preguntacuotas <3){
-    alert ("El costo por mes es de 5.000 pesos durante dos meses")
-}
-else if (preguntacuotas <4){
-    alert ("El costo por mes es de 3.333 pesos durante tres meses")
-}
-else if (preguntacuotas <5){
-    alert ("El costo por mes es de 2.500 pesos durante cuatro meses")
-}
-else if (preguntacuotas <6){
-    alert("El costo por mes es de 2.000 pesos durante cinco meses")
-}
-else if (preguntacuotas <7){
-    alert("El costo por mes es de 1.666 pesos durante seis meses")
-}
-else {
-    alert("El maximo de cuotas posible es 6")
-}
+//creo la funcion para calcular cuotas
+function calcularCostoPorCuotas(pisos, cuotas) {
+    let costoPorCuota;
+
+    switch (pisos) {
+        case 1:
+            if (cuotas === 1) {
+                costoPorCuota = 7000;
+            } else if (cuotas === 2) {
+                costoPorCuota = 3500;
+            } else if (cuotas === 3) {
+                costoPorCuota = 2333;
+            } else {
+                alert("El máximo de cuotas posible es 3");
+            }
             break;
-        case "2":
-            alert ("Eligio la torta de dos pisos")
-            let preguntacuotasdos = prompt("En cuantas cuotas desea la compra?");
-if (preguntacuotasdos < 2) {
-    alert ("El costo en una sola cuota es de 15.000 pesos")
-    
-} 
-else if (preguntacuotasdos <3){
-    alert ("El costo por mes es de 7.000 pesos durante dos meses")
-}
-else if (preguntacuotasdos <4){
-    alert ("El costo por mes es de 5.000 pesos durante tres meses")
-}
-else if (preguntacuotasdos <5){
-    alert ("El costo por mes es de 3.750 pesos durante cuatro meses")
-}
-else if (preguntacuotasdos <6){
-    alert("El costo por mes es de 3.000 pesos durante cinco meses")
-}
-else if (preguntacuotasdos <7){
-    alert("El costo por mes es de 2.500 pesos durante seis meses")
-}
-else {
-    alert("El maximo de cuotas posible es 6")
-}
+        case 2:
+            if (cuotas === 1) {
+                costoPorCuota = 10000;
+            } else if (cuotas === 2) {
+                costoPorCuota = 5000;
+            } else if (cuotas === 3) {
+                costoPorCuota = 3333;
+            } else {
+                alert("El máximo de cuotas posible es 3");
+            }
             break;
         default:
-            alert ("No es posible esa cantidad de pisos")
-            break;
+            alert("No es posible esa cantidad de pisos");
     }
 
+    if (costoPorCuota) {
+        alert("El costo por mes es de " + costoPorCuota + " pesos durante " + cuotas + " meses.");
+    }
 }
 
+//creo la funcion para elegir torta
+function comprarTorta() {
+    let pisos = parseInt(prompt("¿De cuántos pisos desea comprar la torta?"));
+
+    switch (pisos) {
+        case 1:
+            alert("Eligió la torta de un piso");
+            let cuotasUno = parseInt(prompt("¿En cuántas cuotas desea la compra?"));
+            //llama a la funcion de calcular cuotas creada anteriormente
+            calcularCostoPorCuotas(1, cuotasUno);
+            break;
+        case 2:
+            alert("Eligió la torta de dos pisos");
+            let cuotasDos = parseInt(prompt("¿En cuántas cuotas desea la compra?"));
+            //llama a la funcion de calcular cuotas creada anteriormente
+            calcularCostoPorCuotas(2, cuotasDos);
+            break;
+        default:
+            alert("No es posible esa cantidad de pisos");
+    }
+}
+
+// Llama a la funcion para iniciar la compra
+comprarTorta();
